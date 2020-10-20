de611bbd73df839d256587aeffe1fdfa771ca633
donghuaibin <donghuaibin@126.com>
2020-10-20 22:53:34 +0800

    初始化项目

---
+++ nio-test/src/main/java/com/example/nio/niotest/NioTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.nio.niotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NioTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NioTestApplication.class, args);
+	}
+
+}
---
+++ nio-test/src/test/java/com/example/nio/niotest/NioTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nio.niotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NioTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
