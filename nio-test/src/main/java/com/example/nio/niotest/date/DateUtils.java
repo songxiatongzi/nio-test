06d1835c85b284380bb994549a0da680e4862f31
songxiatongzi <donghuaibin@126.com>
2020-10-21 18:41:13 +0800

    日期

---
+++ nio-test/src/main/java/com/example/nio/niotest/date/DateUtils.java
@@ -0,0 +1,31 @@
+package com.example.nio.niotest.date;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Date;
+
+/**
+ * 日期
+ * @author songxia tongzi
+ * @date 2020/10/21 17:34
+ */
+public class DateUtils {
+
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        int year = now.getYear();
+        int monthValue = now.getMonthValue();
+        Month month = now.getMonth();
+        int value = month.getValue();
+        System.out.println(
+                value
+        );
+        System.out.println(monthValue);
+        System.out.println(year);
+        System.out.println(now.toString());
+
+        String localDateStr = LocalDate.of(2020, 10, 19).toString();
+        System.out.println(localDateStr);
+
+    }
+}
