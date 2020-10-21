94020d2c65100563d4256cf4f581b615b2aae7f3
songxiatongzi <donghuaibin@126.com>
2020-10-21 12:20:11 +0800

    添加用户标识

--- nio-test/src/main/java/com/example/nio/niotest/NioTestApplication.java
+++ nio-test/src/main/java/com/example/nio/niotest/NioTestApplication.java
@@ -3,6 +3,10 @@ package com.example.nio.niotest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * NIO-test
+ * @author songxiatongzi
+ */
 @SpringBootApplication
 public class NioTestApplication {
 
